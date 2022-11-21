0573aa36841b5fa4c284009a045ce0a1891395d3
Mairon Florez <andresflorez0799@gmail.com>
2022-11-21 00:37:59 -0500

    add method with take and skip

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,6 +1,11 @@
 using System.Text.Json;
 public class LinqQueries
 {
+    public enum ORDER_DIRECTION
+    {
+        ASC,
+        DESC
+    }
     private readonly List<Book> _librosLt;
     private const string FILE_PATH = "books.json";
 
@@ -24,9 +29,17 @@ public class LinqQueries
 
     //... Muestra por consola el titulo, paginas y fecha de publicacion
     public void ImprimirBooks() => this._librosLt.ForEach(x => Console.WriteLine(x.ToString()));
+    public bool GetFilterAny(Func<Book, bool> filtro) => this._librosLt.Any(filtro);
 
     //... Obtiene un listado de libros filtrado por una expresion lambda "x => x.Field"
-    public IEnumerable<Book> GetCustomFilter(Func<Book, bool> filtro)
-        => this._librosLt.Where(filtro).OrderBy(x => x.PublishedDate);
-
+    public IEnumerable<Book> GetCustomFilter(Func<Book, bool> filtro) => this._librosLt.Where(filtro);
+    public IEnumerable<Book> GetCustomFilter(Func<Book, bool> filtro, Func<Book, object> orden) => this._librosLt.Where(filtro).OrderBy(orden);
+    public IEnumerable<Book> GetCustomFilter(Func<Book, bool> filtro, Func<Book, object> orden, int take, int skip)
+    {
+        return this._librosLt
+            .Where(filtro)
+            .OrderByDescending(orden)
+            .Take(take)
+            .Skip(skip);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,12 +1,18 @@
 ﻿LinqQueries queries = new LinqQueries();
-//Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "TITULO", "PAGINAS", "PUBLICACION");
 //queries.ImprimirBooks();
 
-//Func<Book, bool> filtro = x => x.PublishedDate.Year >= 2000 && x.PageCount > 300;
-Func<Book, bool> filtro = x => x.PageCount > 250
-                               && x.Title != null && x.Title.Contains("in Action");
-var filtrado = queries.GetCustomFilter(filtro);
+Func<Book, bool> filtroAnioAndPages = x => x.PublishedDate.Year >= 2000 && x.PageCount > 300;
+Func<Book, bool> filtroPagesAndTitle = x => x.PageCount > 250 && x.Title != null && x.Title.Contains("in Action");
+Func<Book, bool> filtroTieneStatus = x => x.Status != string.Empty;
+Func<Book, bool> filtroPublicado2005 = x => x.PublishedDate.Year == 2005;
+Func<Book, bool> filtroPython = x => x.Categories != null && x.Categories.Contains("Python");
+Func<Book, bool> filtroJava = x => x.Categories != null && x.Categories.Contains("Java");
+
+var filtradoAny = queries.GetFilterAny(filtroPublicado2005);
+var datosFiltrado = queries.GetCustomFilter(x => x.PageCount > 400, x => x.PublishedDate, 4, 2);
 
 Console.WriteLine("{0, -60} {1, 15} {2, 15}\n", "TITULO", "PAGINAS", "PUBLICACION");
-foreach (var i in filtrado)
+foreach (var i in datosFiltrado)
     Console.WriteLine(i.ToString());
+
+//Console.WriteLine($"Algun libro publicado en 2005: {filtradoAny}");
